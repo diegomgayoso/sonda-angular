02fc5c134f0e32782ee25bfd1c10c730d571e5bd
diegocgayoso <diegoc.gayoso@gmail.com>
2024-10-18 18:07:17 -0300

    refactor: add login page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input'; import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from '../../components/header/header.component';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ MatButtonModule, MatFormFieldModule, MatInputModule, HeaderComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
