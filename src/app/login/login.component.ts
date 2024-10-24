52bf4f91a7dc4ee7995169c2693f82679ff395a6
diegocgayoso <diegoc.gayoso@gmail.com>
2024-10-24 10:32:10 -0300

    feat: criando fluxo de login com ReactiveForm

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,14 +3,23 @@ import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input'; import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from '../components/header/header.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ MatButtonModule, MatFormFieldModule, MatInputModule, HeaderComponent],
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, HeaderComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  userForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  })
 
+  login(){
+    console.log(this.userForm.value);
+
+  }
 }
