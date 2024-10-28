a64746fc5c518bd99fba79d572330a7c92cdcd45
diegocgayoso <diegoc.gayoso@gmail.com>
2024-10-28 14:16:07 -0300

    style: refatorando css das paginas, add ajustes e implementação da tela de atestados.

--- src/app/main/official/official.component.ts
+++ src/app/main/official/official.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-official',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './official.component.html',
   styleUrl: './official.component.scss'
 })
 export class OfficialComponent {
+  public innerWidth: any;
 
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
 }
