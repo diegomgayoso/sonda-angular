f0391000a80c368b2b14bc2c9792da7d8e2f0d14
diegocgayoso <diegoc.gayoso@gmail.com>
2024-10-28 21:56:15 -0300

    style: estilizando o Edital e Atestados Pages com pequenos ajustes o Main.Scss

--- src/app/main/sicknotes/sicknotes.component.ts
+++ src/app/main/sicknotes/sicknotes.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { PaginatorComponent } from "../../components/list/paginator/paginator.component";
+
+
 @Component({
   selector: 'app-sicknotes',
   standalone: true,
-  imports: [],
+  imports: [MatDividerModule, MatFormFieldModule, MatInputModule, MatIconModule, MatInputModule, MatListModule, MatPaginatorModule, PaginatorComponent],
   templateUrl: './sicknotes.component.html',
   styleUrl: './sicknotes.component.scss'
 })
